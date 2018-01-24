0492d802aa1e1355c814eb1ad2c3c9f94eb5a245
Krzysztof Klimonda <kklimonda@syntaxhighlighted.com>
2018-01-24 13:30:54 +0100

    Test fixes to contrail_packaging.py

--- roles/contrail-common-libs/library/contrail_packaging.py
+++ roles/contrail-common-libs/library/contrail_packaging.py
@@ -25,16 +25,14 @@ def main():
     module = AnsibleModule(
         argument_spec=dict(
             zuul=dict(type='dict', required=True),
-            release_type=dict(type='str', required=False, default=ReleaseType.CONTINUOUS_INTEGRATION)
+            release_type=dict(type='str', required=True),
         )
     )
 
     zuul = module.params['zuul']
     release_type = module.params['release_type']
-
     branch = zuul['branch']
-    change = zuul['change']
-    patchset = zuul['patchset']
+
     date = datetime.now().strftime("%Y%m%d%H%M%S")
 
     version = {'epoch': None}
@@ -45,6 +43,8 @@ def main():
 
     if release_type == ReleaseType.CONTINUOUS_INTEGRATION:
         # Versioning in CI consists of change id, pachset and date
+        change = zuul['change']
+        patchset = zuul['patchset']
         version['debian'] = "~{change}.{patchset}~{date}".format(
             change=change, patchset=patchset, date=date
         )
